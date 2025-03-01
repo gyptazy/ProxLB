9eee27afa42560e9596b58bc28fbe14edbd84f7b
gyptazy <gyptazy@gyptazy.com>
2025-03-01 10:03:00 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="python3-proxlb",
+    name="proxlb",
     version="1.1.0",
-    description="My Python Package",
-    long_description="My Python Package",
+    description="A DRS alike loadbalancer for Proxmox clusters.",
+    long_description="An advanced DRS alike loadbalancer for Proxmox clusters that also supports maintenance modes and affinity/anti-affinity rules.",
     author="Florian Paul Azim Hoberg (gyptazy)",
     author_email="gyptazy@gyptazy.com",
     packages=["proxlb", "proxlb.utils", "proxlb.models"],
