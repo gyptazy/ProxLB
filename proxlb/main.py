6b2e1207394b30c0e6d0ce1d1b286af414ff5aa9
Florian Paul Azim Hoberg <florian.hoberg@credativ.de>
2025-06-24 10:06:28 +0200

    Fix: Fixed an issue where balancing was performed in combination of deactivated balancing and dry-run mode
    Fixes: #248

--- proxlb/main.py
+++ proxlb/main.py
@@ -87,8 +87,9 @@ def main():
         Helper.log_node_metrics(proxlb_data, init=False)
 
         # Perform balancing actions via Proxmox API
-        if not cli_args.dry_run or not proxlb_data["meta"]["balancing"].get("enable", False):
-            Balancing(proxmox_api, proxlb_data)
+        if proxlb_data["meta"]["balancing"].get("enable", False):
+            if not cli_args.dry_run:
+                Balancing(proxmox_api, proxlb_data)
 
         # Validate if the JSON output should be
         # printed to stdout
