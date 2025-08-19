8630333e4b445b482fcac38b2bc555a2afbc10e6
gyptazy <gyptazy@gyptazy.com>
2025-08-19 06:27:38 +0200

    feature: Add Helm chart support for ProxLB
    Fixes: #281

--- misc/01-replace-version.sh
+++ misc/01-replace-version.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 VERSION="1.1.4"
 
+# ProxLB
 sed -i "s/^__version__ = .*/__version__ = \"$VERSION\"/" "proxlb/utils/version.py"
 sed -i "s/version=\"[0-9]*\.[0-9]*\.[0-9]*\"/version=\"$VERSION\"/" setup.py
+
+# Helm Chart
+sed -i "s/^version: .*/version: \"$VERSION\"/" helm/proxlb/Chart.yaml
+sed -i "s/^appVersion: .*/appVersion: \"v$VERSION\"/" helm/proxlb/Chart.yaml
+
 echo "OK: Versions have been sucessfully set to $VERSION"
